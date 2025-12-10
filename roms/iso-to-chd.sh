8855f224dfe454674fa6b277fb4c10a34e90d3cd
Junian Triajianto <author@junian.dev>
2025-12-10 14:09:16 +0700

    Add iso to chd script

---
+++ roms/iso-to-chd.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+# Convert .iso to .chd then remove .iso
+find . -name "*.iso" -exec chdman createcd -i {} -o {}.chd \; -exec rm {} \;
+
+# Rename .iso.chd to .chd
+for f in *.iso.chd; do mv -- "$f" "${f%.iso.chd}.chd"; done
+
+# Convert .cue to .chd then remove .cue
+find . -name "*.cue" -exec chdman createcd -i {} -o {}.chd \; -exec rm {} \;
+
+# Rename .cue.chd to .chd
+for f in *.cue.chd; do mv -- "$f" "${f%.cue.chd}.chd"; done
+
+# Remove .bin
+find . -name "*.bin" -exec rm {} \;
